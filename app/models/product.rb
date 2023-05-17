2f2ad819eed44765e576eb1a688a077c8c3f00db
Ekpenisi Erue Raphael <ekpenisiraphael@gmail.com>
2023-05-18 03:41:25 +0530

    Association implemented for category, subcategory and product tables.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
 
-    has_many :reviews, dependent: :destroy
-    has_many :orders, dependent: :destroy
     belong_to :vendors, dependent: :destroy, class_name: 'Vendor'
     belong_to :categories, dependent: :destroy, class_name: 'Category'
-
+    belongs_to :subcategory, dependent: :destroy, class_name: 'Category'
+    has_many :reviews, dependent: :destroy
+    has_many :orders, dependent: :destroy
 end
--- db/migrate/20230517185932_create_products.rb
+++ db/migrate/20230517185932_create_products.rb
@@ -3,10 +3,10 @@ class CreateProducts < ActiveRecord::Migration[7.0]
     create_table :products do |t|
       t.references :vendor, null: false, foreign_key:  {to_table: :vendors}
       t.references :category, null: false, foreign_key:  {to_table: :categories}
+      t.references :subcategory, null: false, foreign_key:  {to_table: :subcategories}
       t.string :name
       t.decimal :price
       t.integer :vendors
-      t.integer :categories
 
       t.timestamps
     end
